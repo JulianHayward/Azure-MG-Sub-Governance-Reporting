d4f9f2d288c418c55eb51472fb6e88df92f1a8d3
Julian Hayward <julian.hayward@microsoft.com>
2022-03-18 16:12:21 +0100

    v6_major_20220318_1

---
+++ pwsh/functions/namingValidation.ps1
@@ -0,0 +1,18 @@
+#region namingValidation
+function NamingValidation($toCheck) {
+    $checks = @(':', '/', '\', '<', '>', '|', '"')
+    $array = @()
+    foreach ($check in $checks) {
+        if ($toCheck -like "*$($check)*") {
+            $array += $check
+        }
+    }
+    if ($toCheck -match '\*') {
+        $array += '*'
+    }
+    if ($toCheck -match '\?') {
+        $array += '?'
+    }
+    return $array
+}
+#endregion namingValidation
