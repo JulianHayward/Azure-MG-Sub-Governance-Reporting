a5e9848782f012c471d01f3ed01c6d4bb598427e
Johan Lindberg <johan.lindberg2@inter.ikea.com>
2026-05-26 16:54:33 +0200

    Add security settings for Azure environments and update API version retrieval

--- pwsh/AzGovVizParallel.ps1
+++ pwsh/AzGovVizParallel.ps1
@@ -710,6 +710,11 @@ Param
             AzureUSGovernment = '2023-01-01'
             AzureChinaCloud   = '2023-01-01'
         }
+        securitySettings    = @{
+            AzureCloud        = '2022-05-01'
+            AzureUSGovernment = '2022-05-01'
+            AzureChinaCloud   = '2021-06-01'   # Use a supported version for China
+        }
     },
 
     [array]
@@ -31463,7 +31468,8 @@ function dataCollectionDefenderPlans {
     }
 
     $currentTask = "Getting Microsoft Defender for Cloud settings for Subscription: '$($scopeDisplayName)' ('$scopeId') [quotaId:'$SubscriptionQuotaId']"
-    $uri = "$($azAPICallConf['azAPIEndpointUrls'].ARM)/subscriptions/$($scopeId)/providers/Microsoft.Security/settings?api-version=2022-05-01"
+    $securitySettingsAPIVersion = $azAPICallConf['htParameters'].APIMappingCloudEnvironment.securitySettings.($azAPICallConf['htParameters'].azureCloudEnvironment)
+    $uri = "$($azAPICallConf['azAPIEndpointUrls'].ARM)/subscriptions/$($scopeId)/providers/Microsoft.Security/settings?api-version=$($securitySettingsAPIVersion)"
     $method = 'GET'
     $securitySettingsResult = AzAPICall -AzAPICallConfiguration $azAPICallConf -uri $uri -method $method -currentTask $currentTask
 
